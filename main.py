693e067fcf1170276ce01b7617604ad3e180b9a9
WaiperOK <86571941+WaiperOK@users.noreply.github.com>
2021-12-26 22:31:07 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+km = int(input("Enter the value in killometers: "))
+
+ratio = 0.621371
+mi=km * ratio
+
+ratio2 = 1000
+m=km*ratio2
+
+ratio3=100000
+cen=km*ratio3
+
+ratio4=1093,61
+yard=km*ratio4
+
+ratio5=3280,83
+foot=km*ratio5
+
+ratio6=39369,96
+inch= km* ratio6
+
+print("The entered value in Miles, Meters, Centimeters, Yards, Foot, Inch: ", mi, m, cen,yard, foot, inch)
